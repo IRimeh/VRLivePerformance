6a04491e6029b0755fb37eff43766ed18f24ab3c
Basboi <SebastiaanEPVorstenbosch@gmail.com>
2020-10-26 13:22:13 +0100

    Imported VFX into Unreal project (particle FX etc)

--- UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Student 2020/Documents/VRLivePerformance/UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/School/SSS/Project/UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,1 @@
+#include "C:/Users/Student 2020/Documents/VRLivePerformance/UnrealVRLivePerformance/UnrealVR/Intermediate/Build/Win64/UnrealVREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
