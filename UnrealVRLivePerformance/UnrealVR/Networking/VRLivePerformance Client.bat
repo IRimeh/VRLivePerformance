889ba69e1d5cf689081530c158f56235e8875001
rpijpker01 <rimme.pijpker@gmail.com>
2020-10-26 14:35:05 +0100

    More multiplayer testing

--- UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
+++ UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
@@ -3,5 +3,5 @@ set ip_address_string="IPv4 Address"
 rem Uncomment the following line when using older versions of Windows without IPv6 support (by removing "rem")
 rem set ip_address_string="IP Address"
 echo Network Connection Test
-for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do "C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\454928\Documents\School\Year 4\SSS\Project\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" %%f -game
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do "C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\Student 2020\Documents\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" %%f -game
 
