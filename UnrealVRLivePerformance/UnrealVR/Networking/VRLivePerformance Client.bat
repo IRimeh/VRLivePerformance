2b37b9ca2b0d568758b0b49243179e43b5c3e6ca
MathieuTeStroete <455237@student.saxion.nl>
2020-11-19 09:39:15 +0100

    Added unison movement from the timeline

--- UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
+++ UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
@@ -3,5 +3,5 @@ set ip_address_string="IPv4 Address"
 rem Uncomment the following line when using older versions of Windows without IPv6 support (by removing "rem")
 rem set ip_address_string="IP Address"
 echo Network Connection Test
-for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do "C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\Student 2020\Documents\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" %%f -game -windowed
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do "C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Mathieu Te Stroete\ICT Schoolwork\Year 3\Minor\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" %%f -game -windowed
 
