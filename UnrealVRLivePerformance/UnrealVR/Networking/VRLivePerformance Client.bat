b5d441dc7012b2c4c04c6c38fd3c8dabf2adb583
rpijpker01 <rimme.pijpker@gmail.com>
2021-01-15 11:59:44 +0100

    Head material customization error fiexed

--- UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
+++ UnrealVRLivePerformance/UnrealVR/Networking/VRLivePerformance Client.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\454928\Documents\School\Year 4\SSS\Project\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" 192.168.178.157 -game -windowed
+"C:\Program Files\Epic Games\UE_4.25\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\454928\Documents\School\Year 4\SSS\Project\VRLivePerformance\UnrealVRLivePerformance\UnrealVR\UnrealVR.uproject" 82.74.155.178 -game -windowed
 
